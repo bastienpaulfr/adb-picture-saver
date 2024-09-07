d7489bd2a9fa5fc55f8b87bf68c267044c091550
bipi <bastien.paul@qonto.com>
2024-09-07 10:30:05 +0200

    First commit

---
+++ sync.sh
@@ -0,0 +1,47 @@
+#!/bin/zsh
+
+# Ensure ADB is installed
+if ! command -v adb &> /dev/null; then
+    echo "ADB is not installed. Please install it first."
+    exit 1
+fi
+
+# Create a base directory to store the images
+BASE_DIR="./Android_Pictures"
+mkdir -p "$BASE_DIR"
+
+# Start the ADB server
+adb start-server
+
+# Wait for the device
+adb wait-for-device
+
+# Pull images from the device
+echo "Pulling images from device..."
+
+FILES=($(adb shell find /sdcard/DCIM/Camera -type f ! -name ".trashed*"))
+
+for FILE in "${FILES[@]}"; do
+    # Get the last modified time of the file from the device
+    echo $FILE
+    MOD_TIME=$(adb shell stat -c %Y "$FILE")
+    
+    LC_TIME=fr_FR.UTF-8
+
+    YEAR_MONTH=$(LC_TIME=$LC_TIME date -r $MOD_TIME +%m)
+    MONTH_NAME=$(LC_TIME=$LC_TIME date -r $MOD_TIME +%B)
+
+    MONTH_NAME=$(echo "$MONTH_NAME" | awk '{print toupper(substr($0,1,1)) tolower(substr($0,2))}')
+
+    FOLDER_NAME="$YEAR_MONTH $MONTH_NAME"
+    
+    echo $FOLDER_NAME
+    # Create a directory for the month if it doesn't exist
+    DEST_DIR="$BASE_DIR/$FOLDER_NAME"
+    mkdir -p "$DEST_DIR"
+    
+    # Pull the file to the appropriate directory
+    adb pull "$FILE" "$DEST_DIR/"
+done
+
+echo "All images have been transferred and organized by month."
